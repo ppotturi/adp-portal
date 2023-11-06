7e4c277fbe20fe5517a6f9a04b3b7b768bf0f029
Logan Talbot <logan.talbot@capgemini.com>
2023-11-06 17:49:55 +0000

    Convert to start to use JSON

--- app/packages/app/src/lib/techradar/techradardata.ts
+++ app/packages/app/src/lib/techradar/techradardata.ts
@@ -252,10 +252,26 @@ import {
 
   export class AdpDataTechRadarApi implements TechRadarApi {
     async load(id: string | undefined) {
-      if (id == 'dev'){
+      var rawData = "https://raw.githubusercontent.com/defra-adp-sandpit/adp-software-templates/tech-radar/tech-radars/development-tech-radar";
+
+      const data = await fetch(rawData).then(res => res.json());
+
+      // For example, this converts the timeline dates into date objects
+      return {
+        ...data,
+        entries: data.entries.map((entry: { timeline: any[]; }) => ({
+          ...entry,
+          timeline: entry.timeline.map(timeline => ({
+            ...timeline,
+            date: new Date(timeline.date),
+          })),
+        })),
+      };
+
+      /*if (id == 'dev'){
         return mock2;
       }else{
         return mock;
-      }
+      }*/
     }
   }
