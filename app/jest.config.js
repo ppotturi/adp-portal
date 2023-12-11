fa675a6cd4b991bdcc0e81a590ce121e250b979b
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2023-12-11 11:36:56 +0000

    added jest config and sonar properties

---
+++ app/jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/*.test.js'
+  ],
+  coverageDirectory: 'test-output',
+  coverageReporters: [
+    'text-summary',
+    'cobertura',
+    'lcov'
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/app/dist/',
+    '<rootDir>/test-output/',
+    '<rootDir>/test/',
+    '<rootDir>/jest.config.js',
+    '<rootDir>/jest.setup.js'
+  ],
+  modulePathIgnorePatterns: [
+    'node_modules'
+  ],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        outputDirectory: 'test-output',
+        outputName: 'junit.xml'
+      }
+    ]
+  ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'node'
+}
