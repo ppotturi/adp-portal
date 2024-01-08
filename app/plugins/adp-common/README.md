62abf44f5c845ea6162fb769404f0830be252eea
Rozerin <rozerin.baran@capgemini.com>
2024-01-08 11:37:17 +0000

    adp-common plugin

---
+++ app/plugins/adp-common/README.md
@@ -0,0 +1,5 @@
+# @internal/plugin-adp-common
+
+Welcome to the common package for the adp plugin!
+
+_This plugin was created through the Backstage CLI_
---
+++ app/plugins/adp-common/src/permissions.ts
@@ -0,0 +1,33 @@
+import { createPermission } from '@backstage/plugin-permission-common';
+
+/**
+ * List of all Programme permissions.
+ *
+ * @public
+ */
+export const adpProgrammmeCreatePermission = createPermission({
+  name: 'adp.programme.create',
+  attributes: { action: 'create' },
+});
+
+/**
+ * List of all Project permissions.
+ *
+ * @public
+ */
+export const adpProjectCreatePermission = createPermission({
+  name: 'adp.project.create',
+  attributes: { action: 'create' },
+});
+
+/**
+ * List of all Project User permissions.
+ *
+ * @public
+ */
+export const adpProjectUserCreatePermission = createPermission({
+  name: 'adp.projectuser.add',
+  attributes: { action: 'create' },
+});
+
+export const todoListPermissions = [adpProgrammmeCreatePermission, adpProjectCreatePermission, adpProjectUserCreatePermission];
