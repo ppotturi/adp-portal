e16dc1cfb1c277d63e59e734186fd6633e5503c9
Rozerin <rozerin.baran@capgemini.com>
2024-01-11 11:07:50 +0000

    fixed comments on pr

--- app/plugins/adp-common/src/index.ts
+++ app/plugins/adp-common/src/index.ts
@@ -1,1 +1,16 @@
+/***/
+/**
+ * Common functionalities for the adp plugin.
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * In this package you might for example declare types that are common
+ * between the frontend and backend plugin packages.
+ */
+/**
+ * Or you might declare some common constants.
+ */
+
 export * from './permissions';
