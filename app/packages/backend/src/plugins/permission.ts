c99672ace8e53b3126ef9dc1c2e3895c3227534a
James Coxhead <james.coxhead@capgemini.com>
2024-02-27 14:06:22 +0000

    fix: Assign scaffolder permissions to programme admins role (#44)

--- app/packages/backend/src/plugins/permission.ts
+++ app/packages/backend/src/plugins/permission.ts
@@ -1,24 +1,29 @@
 import { createRouter } from '@backstage/plugin-permission-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { AdpPortalPermissionPolicy, RbacUtilities, RbacGroups } from '../permissions';
-import {} from '../permissions'
+import {
+  AdpPortalPermissionPolicy,
+  RbacUtilities,
+  RbacGroups,
+} from '../permissions';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-
   const rbacGroups: RbacGroups = {
     platformAdminsGroup: env.config.getString('rbac.platformAdminsGroup'),
     programmeAdminGroup: env.config.getString('rbac.programmeAdminGroup'),
-    adpPortalUsersGroup: env.config.getString('rbac.adpPortalUsersGroup')
-  }
+    adpPortalUsersGroup: env.config.getString('rbac.adpPortalUsersGroup'),
+  };
 
   return await createRouter({
     config: env.config,
     logger: env.logger,
     discovery: env.discovery,
-    policy: new AdpPortalPermissionPolicy(new RbacUtilities(env.logger, rbacGroups), env.logger),
+    policy: new AdpPortalPermissionPolicy(
+      new RbacUtilities(env.logger, rbacGroups),
+      env.logger,
+    ),
     identity: env.identity,
   });
 }
