3461f1a0d0a32626fdab4f0c82f001ee4a9706e1
Danny May <dannyjmay97@gmail.com>
2024-08-07 11:36:47 +0100

    409357: Scaffolder autocomplete accessibility fix (#230)
    * Forward props from the autocomplete to the listbox container
    
    * Bump version

--- plugins/scaffolder-adp-react/src/components/VirtualizedListbox.tsx
+++ plugins/scaffolder-adp-react/src/components/VirtualizedListbox.tsx
@@ -1,6 +1,6 @@
 // source: https://github.com/backstage/backstage/blob/v1.28.4/plugins/scaffolder/src/components/fields/VirtualizedListbox.tsx
 
-import React from 'react';
+import React, { createContext, useContext } from 'react';
 import { FixedSizeList, type ListChildComponentProps } from 'react-window';
 
 const renderRow = (props: ListChildComponentProps) => {
@@ -8,29 +8,37 @@ const renderRow = (props: ListChildComponentProps) => {
   return React.cloneElement(data[index], { style });
 };
 
+const outerItemContext = createContext({});
+const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = useContext(outerItemContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
 export const VirtualizedListbox = React.forwardRef<
   HTMLDivElement,
   { children?: React.ReactNode }
->((props, ref) => {
+>(function VirtualizedListbox(props, ref) {
+  const { children, ...rest } = props;
   const itemData = React.Children.toArray(props.children);
   const itemCount = itemData.length;
 
   const itemSize = 36;
-
-  const itemsToShow = Math.min(10, itemCount);
-  const height = Math.max(itemSize, itemsToShow * itemSize - 0.5 * itemSize);
+  const height = Math.min(itemSize * 10.5, itemSize * (itemCount + 0.5));
 
   return (
     <div ref={ref}>
-      <FixedSizeList
-        height={height}
-        itemData={itemData}
-        itemCount={itemCount}
-        itemSize={itemSize}
-        width="100%"
-      >
-        {renderRow}
-      </FixedSizeList>
+      <outerItemContext.Provider value={rest}>
+        <FixedSizeList
+          height={height}
+          itemData={itemData}
+          itemCount={itemCount}
+          itemSize={itemSize}
+          outerElementType={OuterElementType}
+          width="100%"
+        >
+          {renderRow}
+        </FixedSizeList>
+      </outerItemContext.Provider>
     </div>
   );
 });
